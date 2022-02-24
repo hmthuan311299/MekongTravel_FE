519317bfc5b058823a232d54e08de303a2facb4f
hmthuan311299 <hmthuan311299@gmail.com>
2022-02-24 14:43:46 +0700

    24-02 vuex

---
+++ src/store.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+import state from './store/state'
+import getters from './store/getters'
+import mutations from './store/mutations'
+import actions from './store/actions'
+import moduleAdmin from './store/admin/moduleAdmin'
+import moduleTechnician from './store/technician/moduleTechnician'
+import moduleUser from './store/user/moduleUser'
+import moduleTouristAttraction from './store/touristAtraction/moduleTouristAttraction'
+const store = new Vuex.Store({
+    state,
+    getters,
+    mutations,
+    actions,
+    modules:{
+        admin: moduleAdmin,
+        technician: moduleTechnician,
+        member: moduleUser,
+        attraction: moduleTouristAttraction
+    }
+  })
+export default store;
